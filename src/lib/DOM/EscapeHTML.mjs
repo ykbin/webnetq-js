ef3c1a8f05b9311819c58edc133dc8f6c0d23cda
Yurii Yakubin <yurii.yakubin@gmail.com>
2024-03-30 23:45:42 +0000

    Extended escapeHTML function

--- src/lib/DOM/EscapeHTML.mjs
+++ src/lib/DOM/EscapeHTML.mjs
@@ -1,4 +1,9 @@
 export default function(unsafe) {
+  if (document) {
+    const template = document.createElement('template');
+    template.textContent = unsafe;
+    return template.innerText;
+  }
   return unsafe
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
