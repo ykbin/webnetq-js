15ed2993b45af5f3c30719fa6efaeda975886c71
Yurii Yakubin <yurii.yakubin@gmail.com>
2024-09-22 18:46:11 +0100

    Added fileSizeToString to FS

--- src/lib/FS/index.mjs
+++ src/lib/FS/index.mjs
@@ -9,7 +9,22 @@ function getExtension(filename) {
   return ((index === -1) ? "" : filename.substring(index));
 }
 
+function fileSizeToString(value) {
+  let size = 1;
+  let suffix = "B";
+  const kSuffixMap = [ "KB", "MB", "GB" ];
+  for (const i in kSuffixMap) {
+    const sz = size * 1024;
+    if (value < sz)
+      break;
+    size = sz;
+    suffix = kSuffixMap[i];
+  }
+  return Math.floor(value / size) + " " + suffix;
+};
+
 export default {
   replaceFileExt,
   getExtension,
+  fileSizeToString,
 };
