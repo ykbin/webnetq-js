172f927c4dd8a8077cff48fdfd5334884cb94464
Yurii Yakubin <119136621+ykbin@users.noreply.github.com>
2025-07-27 14:27:24 +0100

    Added Types (#5)

---
+++ src/index.d.ts
@@ -0,0 +1,51 @@
+declare module "webnetq-js" {
+
+namespace NQDOM {
+
+function documentReady(callback: (args: any) => void): void;
+function createElement(html: string): HTMLElement | undefined;
+function getElementByClassName(element: HTMLElement, className: string): HTMLElement | undefined;
+function escapeHTML(str: string): string;
+function setTextContent(val: HTMLElement | string, text: string): void;
+
+} // namespace NQDOM
+
+type ControlConstructor<T extends BaseControl> = new (...args: any[]) => T;
+
+export abstract class ControlManager {
+  getInstance(): ControlManager;
+
+  createControl<T extends BaseControl>(param: ControlConstructor<T> | string): BaseControl;
+  appendControl<T extends BaseControl>(control: T): void;
+  removeControl<T extends BaseControl>(control: T): void;
+  getControl<T extends BaseControl>(param: ControlConstructor<T> | string): BaseControl;
+  getControles<T extends BaseControl>(param: ControlConstructor<T> | string): BaseControl;
+  register<T extends BaseControl>(ctor: ControlConstructor<T>, template: any): void;
+
+  addEventListener(type: string, listener: (event: any) => void): void;
+}
+
+export abstract class BaseControl {
+  get element(): HTMLElement;
+  get dataset(): DOMStringMap;
+  get template(): any;
+
+  show(): void;
+  hide(): void;
+
+  getControl<T extends BaseControl>(param: ControlConstructor<T> | string): BaseControl | undefined;
+  getControles<T extends BaseControl>(param: ControlConstructor<T> | string): BaseControl[];
+
+  setManager(manager: ControlManager): void;
+  insertControl(position: number, control: BaseControl, attachControl?: BaseControl): void;
+  appendControl(control: BaseControl, attachControl?: BaseControl): void;
+  removeControl(control: BaseControl): void;
+  replaceChildren(): void;
+  remove(): void;
+
+  registerEvent(...args: any[]): void;
+  dispatchEvent(type: string, event: any): void;
+  addEventListener(type: string, listener: (event: any) => void): void;
+}
+
+} // module "webnetq-js"
--- src/lib/DOM/GetElementByClassName.mjs
+++ src/lib/DOM/GetElementByClassName.mjs
@@ -1,6 +1,6 @@
 export default function(element, className) {
   if (!className)
-    return null;
+    return undefined;
   if (element.classList.contains(className))
     return element;
   return element.querySelector("." + className);
